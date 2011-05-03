63e29c0d938f772c401cb483c90c84c837e71433
Tim Watson <watson.timothy@gmail.com>
2011-05-03 16:55:01 +0100

    Spike an initial prototype

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Rebar Dist Plugin
 
 This plugin allows you to package up a (configurable) set of files into a
-compressed archive (i.e. .tar.gz or .zip). The plugin's rebar configuration 
+compressed archive (i.e. .tar.gz or .zip). The plugin's rebar configuration
 supports wildcards in both relative and absolute paths for files and directories,
 output path/file name and will allow you to make multiple files if so required.
 
-The plugin exports a new `dist` command, an will also run in response to the 
+The plugin exports a new `dist` command, an will also run in response to the
 `generate` and `clean` commands, although this behaviour is configurable (e.g. if
 you wish to ignore the `generate` command and only run when `dist` is explicitly
 given on the command line).
@@ -14,17 +14,26 @@ given on the command line).
 
 Include the following tuple in your rebar deps:
 
-    {deps, [{'rebar-dist-plugin', ".*", {git, 
+    {deps, [{'rebar-dist-plugin', ".*", {git,
         "git://github.com/hyperthunk/rebar-dist-plugin.git", "master"}}]}.
 
 Then you will be able to fetch and install the plugin (locally) with rebar:
 
-    rebar get-deps compile
+    user@host$ rebar get-deps compile
+    user@host$ rebar dist skip_deps=true
+
+Alternatively, you may put the plugin into your `ERL_LIBS` path somewhere and 
+use it in many projects. This can be done manually, or using a package manager:
+
+    user@host$ epm install hyperthunk/rebar-dist-plugin  # or
+    user@host$ sutro install hyperthunk/rebar-dist-plugin  # or
+    user@host$ agner install rebar-dist-plugin
 
 ## Usage
 
 Configure the dist plugin in your `rebar.config` as usual:
 
+    {rebar_plugins, [rebar_dist_plugin]}.
     {dist, [Options]}.
 
 See the [wiki](https://github.com/hyperthunk/rebar-dist-plugin/wiki) for details
@@ -33,4 +42,4 @@ on the various options (and commands).
 You execute the plugin like any other rebar command:
 
     rebar clean generate    # if you're hooked into a release
-    rebar dist              # if you wish to explicitly assemble a distribution
+    rebar distclean dist    # if you wish to explicitly assemble a distribution
--- src/rebar_dist_plugin.erl
+++ src/rebar_dist_plugin.erl
@@ -26,27 +26,253 @@
 %% @copyright (c) Tim Watson, 2011
 %% @since: April 2011
 %%
-%% @doc Rebar Dist Plugin. 
+%% @doc Rebar Dist Plugin.
 %%
+%% This plugin allows you to package up a (configurable) set of files into a
+%% compressed archive (i.e. .tar.gz or .zip). The plugin's rebar configuration
+%% supports wildcards in both relative and absolute paths for files and
+%% directories, output path/file name and will allow you to make multiple files
+%% if so required.
+%%
+%% The plugin exports a new `dist` command, an will also run in response to the
+%% `generate` and `clean` commands, if configured to do so.
 %% -----------------------------------------------------------------------------
 -module(rebar_dist_plugin).
 
--export([dist/2, distclean/2]).
+-include_lib("kernel/include/file.hrl").
 
+-export([dist/2, distclean/2]).
 -export([generate/2, clean/2]).
 
-generate(Config, _) ->
-    rebar_log:log(debug, "Dist Plugin `generate' ~p~n", [Config]),
-    ok.
+-define(DEBUG(Msg, Args),
+    rebar_log:log(debug, "[~p] " ++ Msg, [?MODULE|Args])).
 
-dist(Config, Thing) ->
-    rebar_log:log(debug, "Dist Plugin `dist' ~p:~p~n", [Thing, Config]),
-    ok.
+-define(WARN(Msg, Args),
+    rebar_log:log(warn, "[~p] " ++ Msg, [?MODULE|Args])).
+
+-record(assembly, {name, opts}).
+-record(conf, {base, rebar, dist}).
+-record(spec, {path, glob, target, mode, template}).
+
+%%
+%% Public API
+%%
+
+dist(Config, AppFile) ->
+    %% AppName will be the default output filename
+    {App, DistConfig} = scope_config(AppFile, Config),
+    dist(#conf{ base=App, rebar=Config, dist=DistConfig }).
 
 distclean(Config, _) ->
     rebar_log:log(debug, "Dist Plugin `distclean' ~p~n", [Config]),
     ok.
 
-clean(Config, _) ->
-    rebar_log:log(debug, "Dist Plugin `clean' ~p~n", [Config]),
-    ok.
+generate(Config, AppFile) ->
+    rebar_log:log(debug, "Dist Plugin `generate' ~p~n", [Config]),
+    Attach = rebar_config:get_local(Config, attach, []),
+    case lists:member(generate, Attach) of
+        true ->
+            dist(Config, AppFile);
+        false ->
+            ok
+    end.
+
+clean(Config, AppFile) ->
+    rebar_log:log(debug, "Dist Plugin `generate' ~p~n", [Config]),
+    Attach = rebar_config:get_local(Config, attach, []),
+    case lists:member(clean, Attach) of
+        true ->
+            distclean(Config, AppFile);
+        false ->
+            ok
+    end.
+
+dist(Conf) ->
+    Results = [ process_assembly(A, Conf) || A <- find_assemblies(Conf) ],
+    Errors = [R || R <- Results, R =/= ok],
+    case (length(Errors) > 0) of
+        true ->
+            {error, Errors};
+        false ->
+            ok
+    end.
+
+%%
+%% Internal API
+%%
+
+process_assembly(#assembly{name=Name, opts=Opts}, GlobalConf) ->
+    ?DEBUG("~p::Config = ~p~n", [Name, Opts]),
+    Format = proplists:get_value(format, Opts, targz),
+    Outdir = proplists:get_value(outdir, Opts, "dist"),
+    InclFiles = proplists:get_value(incl_files, Opts, []),
+    InclDirs = proplists:get_value(incl_dirs, Opts, []),
+    ExclFiles = proplists:get_value(excl_files, Opts, []),
+    ExclDirs = proplists:get_value(excl_dirs, Opts, []),
+    CopyDirs = filter(collect_dirs(InclDirs, Opts),
+                      flatten_specs(collect_dirs(ExclDirs, Opts))),
+    CopyFiles = filter(collect_files(InclFiles, Opts),
+                       flatten_specs(collect_files(ExclFiles, Opts))),
+    MergedFsEntries = merge_entries(Format, CopyDirs, CopyFiles, Opts),
+    generate_assembly(Format, Name, Outdir, MergedFsEntries).
+
+generate_assembly(zip, Name, Outdir, MergedFsEntries) ->
+    ensure_path(Outdir),
+    Opts = case rebar_config:get_global(verbose, false) of
+        true ->
+            [verbose];
+        false ->
+            []
+    end,
+    Filename = filename:join(Outdir, Name) ++ ".zip",
+    %% TODO: we must *remove* any duplicate entries (prefix based)
+    {ok, {OutputFile, Archive}} = 
+        zip:create(Filename, MergedFsEntries, Opts ++ [memory]),
+    %% TODO: reconstruct the template entries in memory before writing to disk
+    error;
+generate_assembly(tar, Name, Outdir, MergedFsEntries) ->
+    ensure_path(Outdir),
+    Opts = case rebar_config:get_global(verbose, false) of
+        true ->
+            [write, compressed, verbose];
+        false ->
+            [write, compressed]
+    end,
+    Filename = filename:join(Outdir, Name) ++ ".tar.gz",
+    erl_tar:create(Filename, MergedFsEntries, Opts).
+
+merge_entries(Format, DirSpecs, FileSpecs, Opts) ->
+    CopyDirs = flatten_specs(DirSpecs),
+    CopyFiles = flatten_specs(FileSpecs),
+    TemplatedFiles = [ F || {S, F} <- CopyFiles, S#spec.template =:= true ],
+    SpecificFiles = [ output_def(S, F, Format) || {S, F} <- CopyFiles,
+                           not lists:any(prefix_of(F), CopyDirs) andalso
+                           S#spec.template =/= true ],
+    ProcessedTemplates = merge_templates(TemplatedFiles, Format, Opts),
+    lists:concat([CopyDirs, SpecificFiles, ProcessedTemplates]).
+
+output_def(Spec, {File, Data}, Format) ->
+    output_def(Spec, File, Data, Format);
+output_def(Spec, File, Format) ->
+    output_def(Spec, File, undefined, Format).
+
+output_def(Spec, File, Data, Format) ->
+    {ok, FI} = file:read_file_info(File),
+    PathInArchive = case Spec#spec.target of
+        undefined ->
+            File;
+        '_' ->
+            File;
+        Target ->
+            case FI#file_info.type of
+                regular ->
+                    NewName = filename:join(Target, filename:basename(File)),
+                    {File, {rewrite, NewName}};
+                _ ->
+                    {File, {rewrite, Target}}
+            end
+    end.
+
+is_template({Spec, _}) ->
+    Spec#spec.template =:= true.
+
+prefix_of(F) ->
+    fun(D) -> lists:prefix(D, F) end.
+
+merge_templates(TemplatedFiles, Format, Opts) ->
+    Vars = dict:from_list(proplists:get_value(vars_file, Opts, [])),
+    lists:map(fun(F) -> apply_template(F, Vars, Format, Opts) end, TemplatedFiles).
+
+apply_template({Spec, File}, Vars, Format, Opts) ->
+    {ok, Bin} = file:read_file(File),
+    output_def(Spec, {File, rebar_templater:render(Bin, Vars)}, Format).
+
+flatten_specs([]) ->
+    [];
+flatten_specs([{Spec, Items}|Rest]) ->
+    [{Spec, I} || I <- Items] ++ flatten_specs(Rest).
+
+filter({Spec, Incl}, Excl) ->
+    {In, Out} = lists:partition(fun(I) -> should_include(I, Excl) end, Incl),
+    ?DEBUG("Excluding ~p~n", [Out]),
+    {Spec, In}.
+
+should_include(Incl, Exclusions) ->
+    not lists:any(fun(Ex) -> lists:prefix(Ex, Incl) end, Exclusions).
+
+collect_dirs(Includes, Opts) ->
+    lists:map(fun process_dir/1, Includes).
+
+collect_files(Includes, Opts) ->
+    Cwd = rebar_utils:get_cwd(),
+    Dirs = lists:duplicate(length(Includes), Cwd),
+    lists:map(fun process_files/1, lists:zip(Dirs, Includes)).
+
+%% TODO: handle *mode* by doing a(ny) chmod/chown/chgrp in a work directory
+
+%% TODO: unify process_files and process_dir
+%% TODO: rename process_dir => process_dirs
+
+process_files({Dir, #spec{path=Path, glob=undefined}=Spec}) ->
+    {Spec, Path};
+process_files({Dir, #spec{glob=Regex}=Spec}) ->
+    {Spec, rebar_utils:find_files(Dir, Regex)};
+process_files({Dir, PathExpr}) when is_list(PathExpr) ->
+    process_files(Dir, #spec{glob=PathExpr}, Opts).
+
+process_dir(#spec{path=Path, glob=undefined}=Spec) ->
+    {Spec, Path};
+process_dir(#spec{glob=Glob}=Spec) ->
+    {Spec, filelib:wildcard(Glob)};
+process_dir(PathExpr) when is_list(PathExpr) ->
+    process_dir(#spec{glob=PathExpr}).
+
+ensure_path(Dir) ->
+    rebar_utils:ensure_dir(Dir),
+    ok = case filelib:is_dir(Dir) of
+        true ->
+            ok;
+        false ->
+            file:make_dir(Dir)
+    end,
+    Dir.
+
+find_assemblies(#conf{ base=Base, rebar=Config, dist=DistConfig }) ->
+    case lists:filter(fun(X) -> element(1, X) == assembly end, DistConfig) of
+        [] ->
+            %% TODO: infer an assembly from *top level* config
+            [#assembly{name=Base, opts=DistConfig}];
+        Assemblies ->
+            Assemblies
+    end.
+
+scope_config(undefined, Config) ->
+    App = {app, basename()},
+    BaseConfig = rebar_config:get_list(Config, dist, []),
+    {App, [App|merge_config(BaseConfig)]};
+scope_config(AppFile, Config) ->
+    App = {app, rebar_app_utils:app_name(AppFile)},
+    BaseConfig = rebar_config:get_list(Config, dist, []),
+    {App, [App|merge_config(BaseConfig)]}.
+
+merge_config(BaseConfig) ->
+    case lists:keyfind(config, 1, BaseConfig) of
+        false ->
+            BaseConfig;
+        ConfigPath ->
+            case file:consult(ConfigPath) of
+               {ok, Terms} ->
+                   %% all non-assembly members are ignored
+                   Assemblies = [ A || A <- Terms, element(1, A) =:= assembly ],
+                   lists:foldl(fun merge_assemblies/2, BaseConfig, Assemblies);
+               Other ->
+                   ?WARN("Failed to load ~s: ~p\n", [ConfigPath, Other]),
+                   BaseConfig
+           end
+    end.
+
+merge_assemblies(E, Acc) ->
+    lists:keyreplace(E#assembly.name, 2, Acc, E).
+
+basename() ->
+    filename:basename(rebar_utils:get_cwd()).
